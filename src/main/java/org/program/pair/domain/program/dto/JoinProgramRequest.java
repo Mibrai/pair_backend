c74fdd67161bbc995cd2dad672575ee7b43f820d
mibrai <53493202+Mibrai@users.noreply.github.com>
2026-07-09 23:01:49 +0200

    fix join program

--- src/main/java/org/program/pair/domain/program/dto/JoinProgramRequest.java
+++ src/main/java/org/program/pair/domain/program/dto/JoinProgramRequest.java
@@ -1,11 +1,8 @@
 package org.program.pair.domain.program.dto;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.util.UUID;
 
 public record JoinProgramRequest(
-    @NotNull UUID programId,
-    UUID scheduleId // Optional: specific schedule to join
+    UUID scheduleId
 ) {
 }
